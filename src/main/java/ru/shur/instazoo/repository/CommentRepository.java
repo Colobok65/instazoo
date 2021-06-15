fa3b6979824981a4a9573bcb313361e971edc560
Colobok65 <84aas@mail.ru>
2021-06-15 17:04:48 +0300

    Created repositories

---
+++ src/main/java/ru/shur/instazoo/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package ru.shur.instazoo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.shur.instazoo.entity.Comment;
+import ru.shur.instazoo.entity.Post;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+}
---
+++ src/main/java/ru/shur/instazoo/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package ru.shur.instazoo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.shur.instazoo.entity.Post;
+import ru.shur.instazoo.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+}
