73745d7f1808b8f63b026136cfedab5b9371e4d2
Colobok65 <84aas@mail.ru>
2021-06-17 12:55:09 +0300

    Create validations, annotations, requests for security

---
+++ src/main/java/ru/shur/instazoo/annotations/PasswordMatches.java
@@ -0,0 +1,18 @@
+package ru.shur.instazoo.annotations;
+
+import ru.shur.instazoo.validations.PasswordMatchesValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Documented
+public @interface PasswordMatches {
+
+    String message() default "Password do not match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/ru/shur/instazoo/validations/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package ru.shur.instazoo.validations;
+
+import ru.shur.instazoo.annotations.PasswordMatches;
+import ru.shur.instazoo.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
