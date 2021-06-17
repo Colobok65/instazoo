e75c6c272086a0e1c31027637ab8ca71cd8c3bc2
Colobok65 <84aas@mail.ru>
2021-06-17 15:39:44 +0300

    Create UserService and createUser method and create exception package

---
+++ src/main/java/ru/shur/instazoo/services/UserService.java
@@ -0,0 +1,45 @@
+package ru.shur.instazoo.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.shur.instazoo.entity.User;
+import ru.shur.instazoo.entity.enums.ERole;
+import ru.shur.instazoo.exceptions.UserExistsException;
+import ru.shur.instazoo.payload.request.SignupRequest;
+import ru.shur.instazoo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(ERole.ROLE_USER);
+
+        try {
+            LOG.info("Saving User {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception e) {
+            LOG.error("Error during registration. {}", e.getMessage());
+            throw new UserExistsException("The user " + user.getUsername() + " already exist. Please check credentials");
+        }
+    }
+}
