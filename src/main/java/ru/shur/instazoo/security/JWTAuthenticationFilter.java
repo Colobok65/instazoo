6cf64354ae9f21eb08603618f2c108d5814db2ee
Colobok65 <84aas@mail.ru>
2021-06-17 11:59:26 +0300

    Create SecurityConfig

--- src/main/java/ru/shur/instazoo/security/JWTAuthenticationFilter.java
+++ src/main/java/ru/shur/instazoo/security/JWTAuthenticationFilter.java
@@ -22,14 +22,10 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     public static final Logger LOG = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
 
-    private final JWTTokenProvider jwtTokenProvider;
-    private final CustomUserDetailsService customUserDetailsService;
-
     @Autowired
-    public JWTAuthenticationFilter(JWTTokenProvider jwtTokenProvider, CustomUserDetailsService customUserDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.customUserDetailsService = customUserDetailsService;
-    }
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
