fbd58210ccf461c5423f1522449c39425ae7feaf
Colobok65 <84aas@mail.ru>
2021-06-23 13:11:48 +0300

    Created notification.service (client)

--- client/src/app/services/notification.service.ts
+++ client/src/app/services/notification.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  showSnackBar(message: string): void {
+    // this.snackBar.open(message, null, {duration: 1000});
+    this.snackBar.open(message, undefined, {
+      duration: 2000 /* сообщение будет показано в течении 2 сек */
+    });
+  }
 }
